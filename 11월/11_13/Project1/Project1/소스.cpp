8f4e20a0d955610427db9273d1eb138c7ae1e534
ohsean93 <jeasuk93@gmail.com>
2019-11-29 09:04:02 +0900

    ssafy_final

--- 11월/11_13/Project1/Project1/소스.cpp
+++ 11월/11_13/Project1/Project1/소스.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-	int num, num2;
-
-	cin >> num >> num2;
-	
-	cout << num << ' ' << num2 << endl;
-
+	cout << "Hello World" << endl;
 	return 0;
 }
